60bd81d595f681c5fcfd59a3c340cf683f9dbb79
Yannis Lagrosa <378940@ga.aliefisd.net>
2022-07-02 20:26:55 -0500

    Updated Files.

--- Game/Assets/HealthManager.cs
+++ Game/Assets/HealthManager.cs
@@ -6,10 +6,11 @@ public class HealthManager : MonoBehaviour
 {
     public UnityEngine.UI.Image healthbar1;
     public UnityEngine.UI.Image healthbar2;
-    public UnityEngine.UI.Image healthbar3;
     public PlayerStats stats;
     public float test;
     public float percenHealth;
+    [Range(0f,1f)]
+    public float lerpalot;
 
     private void Start()
     {
@@ -19,6 +20,8 @@ public class HealthManager : MonoBehaviour
     private void Update()
     {
     }
+
+    //OPTIMIZATION: Make both of the healthbars use the same function.
     public void updateHealthBar(UnityEngine.UI.Image healthbar, float a, float b, float t)
     {
         if (t < 0)
@@ -29,6 +32,14 @@ public class HealthManager : MonoBehaviour
         float lerped = Mathf.Lerp(a, b, t);
         healthbar.rectTransform.sizeDelta = new Vector2(t * healthbar.rectTransform.sizeDelta.x, healthbar.rectTransform.sizeDelta.y);
         healthbar.rectTransform.localPosition = new Vector3(-150, healthbar.rectTransform.localPosition.y, healthbar.rectTransform.localPosition.z);
-        
+        healthFX();
     }
+
+    /*public void healthFX()
+    {
+        healthbar.rectTransform.sizeDelta = new Vector2(t * healthbar.rectTransform.sizeDelta.x, healthbar.rectTransform.sizeDelta.y);
+
+        healthbar2.rectTransform.localPosition = new Vector3(-150, healthbar2.rectTransform.localPosition.y, healthbar2.rectTransform.localPosition.z);
+        lerpalot = 0;
+    }*/
 }
